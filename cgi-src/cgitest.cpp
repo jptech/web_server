78a55bbdef956d116f9964c4481e1c3cc113df39
Nick Skuda <nskuda@vols.utk.edu>
2018-03-02 20:24:19 -0500

    updated cgitest and the cgi launch to take arguments

--- cgi-src/cgitest.cpp
+++ cgi-src/cgitest.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {
   cout << "Hello I am a CGI test script \n";
+  if(argc >= 2)
+  {
+    cout << "The Square-Root of " << atoi(argv[1]) << " is " << sqrt(atoi(argv[1])) << endl;
+  }
 }
